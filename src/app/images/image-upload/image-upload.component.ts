cc43d2244c1a197e432ca8807aeb17ef9d3f5810
Todor's Laptop <todor_an@yahoo.com>
2017-05-17 07:58:46 +0300

    image upload fn

--- src/app/images/image-upload/image-upload.component.ts
+++ src/app/images/image-upload/image-upload.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ImagesService } from '../images.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/forkJoin';
 
 @Component({
   selector: 'app-image-upload',
@@ -19,75 +18,24 @@ export class ImageUploadComponent implements OnInit {
   ngOnInit() {
   }
 
-  getBase64(file) {
+  upload(file: File) {
     const reader = new FileReader();
-
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-      console.log(reader.result);
-    };
-
-    reader.onerror = function (error) {
-      console.log('Error: ', error);
-    };
-  }
-
-  upload(e: any): void {
-    const imageFile = this.getBase64(e);
-    console.log('Image file: ', imageFile);
-    this.imagesService.upload(this.getBase64(e)).subscribe(
-      image => {
-        this.router.navigate(['/images']);
-      }
-    );
-  }
-
-  onChange(event) {
-    const files = event.srcElement.files;
-    this.imagesService.makeFileRequest([], files).subscribe(() => {
-      console.log('sent');
-    });
-  }
-  // example that should be working already
-  onChange2($event: any) {
-    console.log('No of files selected: ' + $event.target.files.length);
-    // Make sure to clear the observableBatch array before restarting the whole process.
-    this.observableBatch = [];
-
-    const rawFiles = $event.target.files;
-    for (let i = rawFiles.length - 1; i >= 0; i--) {
-      this.setUpFile(rawFiles[i]);
-    }
-    Observable.forkJoin(this.observableBatch)
-      .subscribe(
-        (m) => {
-          console.log(m);
-        },
-        (e) => {
-          console.log(e);
-        },
-        () => {
-          console.log('All file(s) loading completed!!!');
-        }
-      );
-  }
-
-  setUpFile(file) {
-    const reader = new FileReader();
-
-    const myobservable = Observable.create((observer: any) => {
+    const fileObservable = Observable.create((observer: any) => {
       reader.onload = function (e: any) {
-        console.log('result: ', e);
-        const data = e.target;
-        const imageSrc = data.result;
-        console.log('File loaded succesfully.');
-        console.log(imageSrc);
-        observer.next(imageSrc);
+        observer.next(e.target.result);
         observer.complete();
       };
-    });
+    }).subscribe(
+      (m) => {
+        this.imagesService.upload({'file': m}).subscribe(() => {
+          console.log('sent');
+        });
+      },
+      (e) => {
+        console.log('error: ', e);
+      }
+    );
 
-    this.observableBatch.push(myobservable);
     reader.readAsDataURL(file);
   }
 }
